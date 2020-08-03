cf07a4795d9f67877ff35688ae7afcc5c5eac614
David Grayboff <dgrayboff@gmail.com>
2020-08-03 18:05:06 -0400

    Updates

---
+++ precache-manifest.92df4d58c97df9792322490c9b7be593.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "789b07501ede561f48d21d2aead422f7",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "e3a3babf616ba33df970",
+    "url": "/react-todo-app/static/css/main.6559de9f.chunk.css"
+  },
+  {
+    "revision": "340b8b3482bfea901121",
+    "url": "/react-todo-app/static/js/2.0e27dce5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.0e27dce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3a3babf616ba33df970",
+    "url": "/react-todo-app/static/js/main.c3a67906.chunk.js"
+  },
+  {
+    "revision": "c32714fa856fe18ed5ee",
+    "url": "/react-todo-app/static/js/runtime-main.ea788152.js"
+  }
+]);
